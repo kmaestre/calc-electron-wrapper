4ec10d9f33a167521906d66b47a79874c5381a1f
Miguel Maestre <maestre95@gmail.com>
2021-07-04 11:27:20 -0400

    validacion de macs locales contra json

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const { app, BrowserWindow, dialog, ipcMain } = require('electron')
 const fs = require('fs')
 const path = require('path')
-const { homedir, hostname } = require('os')
-let validationPath = path.join(homedir(), '/AppData/CalculadoraApuestas/')
-let validationFilePath = path.join(validationPath, 'application.json')
+const { homedir, hostname, networkInterfaces } = require('os')
+const { execSync } = require('child_process')
+const validationPath = path.join(homedir(), '/AppData/CalculadoraApuestas/')
+const validationFilePath = path.join(validationPath, 'application.json')
 
 function createMainWindow() {
   const win = new BrowserWindow({
@@ -19,6 +20,22 @@ function createMainWindow() {
   win.once('ready-to-show', () => {
     win.show()
   })
+}
+
+function getMACs() {
+  return execSync('getmac /fo csv /nh', { encoding: 'utf-8' }).match(/\"(([A-Z0-9]{2,2})\-)+([A-Z0-9]{2,2})\"/g).join(',').replaceAll('"', '').split(',')
+}
+
+function compareMACs(jsonMACs) {
+  let pc = getMACs()
+  let res = false
+  let i = 0
+  while (!res) {
+    res = (jsonMACs.indexOf(pc[i]) >= 0) ? true : false
+    i++
+  }
+
+  return res
 }
 
 function createActivationWindow() {
@@ -41,9 +58,11 @@ function createActivationWindow() {
   win.once('ready-to-show', () => {
     win.show()
   })
+
   ipcMain.handle('successfull-activation', (event, { email, pcName }) => {
+    let macs = getMACs()
     if (!fs.existsSync(validationPath)) fs.mkdirSync(validationPath)
-    fs.writeFileSync(validationFilePath, `{"email":"${email}","deviceName":"${pcName}"}`)
+    fs.writeFileSync(validationFilePath, `{"email":"${email}","deviceName":"${pcName}", "macs": ${JSON.stringify(macs)}}`)
     createMainWindow()
     win.close()
   })
@@ -56,9 +75,11 @@ app.on('window-all-closed', function () {
 app.whenReady().then(() => {
   if (!fs.existsSync(validationFilePath)) createActivationWindow()
   else {
-    let validationData = fs.readFileSync(validationFilePath)
-    if (validationData && JSON.parse(validationData).deviceName == hostname()) createMainWindow()
-    else {
+    let validationData = JSON.parse(fs.readFileSync(validationFilePath))
+    
+    if (validationData && validationData.deviceName == hostname() && compareMACs(validationData.macs)) {
+      createMainWindow()
+    } else {
       dialog.showErrorBox('Error!', 'mamalo perro')
       app.quit()
     }
